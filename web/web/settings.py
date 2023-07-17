fac1bab49f83285311aa2542c78cc813a3c3c0e4
tidi18 <animeparene@gmail.com>
2023-07-17 16:01:40 +0600

    переделал сбросс пароля

--- web/web/settings.py
+++ web/web/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 # ...
 
 # Настройки для отправки электронной почты через Gmail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = ''
